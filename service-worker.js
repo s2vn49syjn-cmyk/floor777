612306c8ec54cea8d47de6e3a5382c1be3f175bc
s2vn49syjn-cmyk <eita3982@icloud.com>
2026-09-19 02:16:18 +0900

    Bump cache for mobile map tools

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='floor777-v1-20260919-diffpalette1';
+const CACHE='floor777-v1-20260919-mobiletools1';
 const CORE=[
   './','./index.html','./halls/','./halls/hyper-arrow-mihara/','./assets/styles.css','./assets/common.js','./assets/home.js','./assets/app.js','./data/halls.json','./data/hyper-arrow-mihara.json','./data/positions-mihara.json','./offline.html'
 ];
