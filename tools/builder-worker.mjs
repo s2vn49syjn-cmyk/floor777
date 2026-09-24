70f5781384b904ce1fb3e82e945659b718cd8efc
s2vn49syjn-cmyk <eita3982@icloud.com>
2026-09-24 20:29:10 +0900

    Remove public floor map builder and all site links (#8)

--- tools/builder-worker.mjs
+++
@@ -1,2 +0,0 @@
-import {detectIslands} from './builder-detect.mjs';
-self.onmessage=e=>{try{self.postMessage({islands:detectIslands(e.data.image,e.data.scale,e.data.options)});}catch(error){self.postMessage({error:error.message});}};
